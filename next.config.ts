45f72d027293e0bb3baebaa721307a9852cb9caf
sphinx-p <chacritpoonpipit@gmail.com>
2025-01-03 10:59:38 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? "/startup-website-nextjs/" : "",
+  basePath: isProd ? "/startup-website-nextjs" : "",
+  output: "export",
 };
 
 export default nextConfig;
